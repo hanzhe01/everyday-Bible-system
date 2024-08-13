696f7aad28cd8839dee0adf44e5487263d6250de
hanzhe01 <3132233056@qq.com>
2024-08-13 11:22:06 +0800

    更换为mybatis-plus

---
+++ Bible-common/src/main/java/com/youlintech/bible/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.youlintech.bible.common.exception.user;
+
+import com.youlintech.bible.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
